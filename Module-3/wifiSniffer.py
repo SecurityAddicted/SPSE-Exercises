9e34717ef23fd2977a2236e01cc800f9688fd086
Alex-SA <alex@securityaddicted.com>
2012-05-22 15:19:13 +0200

    fixed some bugs

--- Module-3/wifiSniffer.py
+++ Module-3/wifiSniffer.py
@@ -8,7 +8,7 @@ bssidList = []
 
 def sniffSsid(pkt):
         global bssidList
-        if pkt.haslayer(Dot11Beacon) or pkt.haslayer(Dot11ProbeResp):
+        if pkt.haslayer(Dot11Beacon):
                 if pkt.addr3 not in bssidList:
                         print pkt.sprintf("%Dot11.addr3%\t%Dot11Beacon.info%\t%Dot11Beacon.cap%")
                         bssidList.append(pkt.addr3)
