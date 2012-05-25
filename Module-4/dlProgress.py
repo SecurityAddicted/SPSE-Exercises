b9e8aa525a23d2771f5d9d156aac51a28a9a71a2
Alex-SA <alex@securityaddicted.com>
2012-05-25 18:07:19 +0200

    module4-video1 exercise completed

---
+++ Module-4/dlProgress.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import sys, urllib, getopt,time
+from progressbar import *
+
+def usage():
+        print "Usage: ./dlProgress.py <url1> <url2> ... <urln>"
+
+def main(argv):
+        try:
+                opts, args = getopt.getopt(argv, "h")
+        except getopt.GetoptError:
+                usage()
+                sys.exit(2)
+        for opt, arg in opts:
+                if opt in ("-h"):
+                        usage()
+                        sys.exit()
+        if len(args) < 1:
+                usage()
+                sys.exit(2)
+        i = 1
+        for url in args:
+                print "Downloading: ", url
+                try:
+                        urllib.urlretrieve(url, str(i), reporthook=dlProgress)
+                except:
+                        print "Error during download of ",url
+                i += 1
+
+def dlProgress(count, blockSize, totalSize):
+        percent = int(count*blockSize*100/totalSize)
+        sys.stdout.write("%2d%%" % percent)
+        sys.stdout.write("\b\b\b")
+        sys.stdout.flush()
+
+if __name__ == "__main__":
+        main(sys.argv[1:])
